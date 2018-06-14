65fd5c6b5e2edb2a681af4125278810cbe867b3b
braelynmuskwe <braemira1@icloud.com>
2018-06-14 21:12:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do 
+    "My name is Braelyn"
+  end
+  get '/hometown' do
+    "My hometown is Illionis"
+  end
+  get '/favorite-song' do
+    "My favorite song is Mine"
+  end
 end
+  
+  
+  
+
